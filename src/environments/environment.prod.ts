95ed86a48558f104b97806d1541c678cb5061b8a
nelrsf <nsantamaria@tiresia.co>
2023-05-01 11:23:10 -0500

    Updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiUrl: 'https://eco-quality-erp-crud-service.vercel.app',
   authUrl: 'https://eco-quality-auth.vercel.app',
   filesUrl: 'https://eq-files.onrender.com',
-  recoveryCodeKey: "SANTI_Y_SILVIA_LOS_AMO"
+  recoveryCodeKey: "210_SANTI_Y_3_SILVIA_2_LOS_AMO_1"
 };
 
 /*
